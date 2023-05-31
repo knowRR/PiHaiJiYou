dfdd0e23f5b442f378a69e5a2cd0cb56cdf4327c
John Doe <johndoe@example.com>
2023-05-31 21:26:06 +0800

    try build1

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,34 @@
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vite'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
+
+export default defineConfig({
+	plugins: [svelte()],
+	build: {
+	  src: './docs', // relative to index.html
+	  emptyOutDir: true, // true if outDir is inside root. if outDir is not inside root, uncomment this.
+	},
+	adapter: adapter({
+		name: 'adapter-package-name'
+	}),
+	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
+	// for more information about preprocessors
+	preprocess: vitePreprocess(),
+
+	kit: {
+		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
+		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
+		adapter: adapter()
+	}
+  })
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	adapter: adapter({
+		name: 'adapter-package-name'
+	}),
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
@@ -15,4 +41,4 @@ const config = {
 	}
 };
 
-export default config;
+// export default config;
