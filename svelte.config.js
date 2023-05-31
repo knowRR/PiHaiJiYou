e675930b3ba60ccc4dc8f006bc3e5b94dda437fb
John Doe <johndoe@example.com>
2023-05-31 21:50:30 +0800

    try build2 use svelte-adapter-github

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from 'svelte-adapter-github';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
